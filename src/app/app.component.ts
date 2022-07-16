42fc1508e924b77e3dcfb04bab3c016f64dab2dc
FilipDavid1 <filipenkodavid@gmail.com>
2022-07-16 18:11:17 +0200

    add select for ordering tasks and add button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoApp';
+
+  ngOnInit() {}
+
+  constructor() {}
+
+  tasks = [
+    {title: 'Task 1', description: 'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription', isDone: false},
+    {title: 'Task 2', description: 'description', isDone: false},
+    {title: 'Task 3', description: 'description', isDone: false},
+    {title: 'Task 4', description: 'description', isDone: false},
+  ];
+
+  selectedValue: string = '';
+
+  filters = [
+    {value: 'all-0', viewValue: 'All'},
+    {value: 'active-1', viewValue: 'Active'},
+    {value: 'completed-2', viewValue: 'Completed'},
+  ];
+
+  
+
+  // addTask(task: string) {
+  //   this.tasks.push(task);
+  // }
+
+  // deleteTask(task: string) {
+  //   this.tasks.splice(this.tasks.indexOf(task), 1);
+  // }
+
+
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatSelectModule,
+    MatExpansionModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
