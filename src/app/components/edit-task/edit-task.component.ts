e0913db99f3de81ca82513ef00a935d50ff9876a
FilipDavid1 <filipenkodavid@gmail.com>
2022-07-18 21:12:22 +0200

    implement task editing

--- src/app/components/edit-task/edit-task.component.ts
+++ src/app/components/edit-task/edit-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Task } from 'src/app/classes/task';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-edit-task',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTaskComponent implements OnInit {
 
-  constructor() { }
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getExercise();
+  }
+
+  loading: boolean = false;
+
+  id: number;
+  task: Task = new Task('', '', '', false);
+
+  getExercise(){
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.taskService.getTask(id).subscribe(response => {
+      this.task = response;
+      this.id = response.id;
+      console.log(response);
+      
+    });
+  }
+
+  onSubmit(){
+    this.loading = true;
+    this.taskService.updateTask(this.task).subscribe(response => {
+      console.log(this.task);
+      
+      this.loading = false;
+    });
   }
 
 }
